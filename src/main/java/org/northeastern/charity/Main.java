7a8035ebc9f0f1e3ff7e2a42fda3b3e01f10a811
Achyuth Varma <achyuthvarma@Achyuths-MacBook-Pro.local>
2022-04-29 22:55:47 -0400

    Created Hibernate Persistence and successfully connected to DB

--- src/main/java/org/northeastern/charity/Main.java
+++ src/main/java/org/northeastern/charity/Main.java
@@ -1,7 +1,42 @@
 package org.northeastern.charity;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import org.northeastern.charity.models.Person;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("org.hibernate.connection.jpa");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+
+        try{
+
+            entityTransaction.begin();
+
+            Person person = new Person(1, "Achyuth");
+
+            entityManager.persist(person);
+
+            entityTransaction.commit();
+
+        }
+        finally {
+            if(entityTransaction.isActive()){
+                entityTransaction.rollback();
+            }
+            entityManager.close();
+            entityManagerFactory.close();
+        }
+
+
+
+
     }
 }
